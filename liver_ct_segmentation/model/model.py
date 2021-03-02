d7e2d52753f0c9c9275efd35378675687a8a134d
luiskuhn <luis.kuhn@gmail.com>
2021-03-02 12:09:17 +0100

    init_

--- liver_ct_segmentation/model/model.py
+++ liver_ct_segmentation/model/model.py
@@ -41,7 +41,7 @@ class LitsSegmentator(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--num-workers', type=int, default=3, metavar='N', help='number of workers (default: 3)')
+        parser.add_argument('--num-workers', type=int, default=2, metavar='N', help='number of workers (default: 2)')
         parser.add_argument('--lr', type=float, default=0.0001, help='learning rate (default: 0.0001)')
         parser.add_argument('--training-batch-size', type=int, default=8, help='Input batch size for training')
         parser.add_argument('--test-batch-size', type=int, default=8, help='Input batch size for testing')
